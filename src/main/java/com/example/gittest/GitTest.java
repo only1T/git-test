91beda519eef5f89585d4da750c5cbe83414b15b
thd <1187843791@qq.com>
2022-10-23 22:28:47 +0800

    first idea commit

---
+++ src/main/java/com/example/gittest/GitTest.java
@@ -0,0 +1,11 @@
+package com.example.gittest;
+
+/**
+ * @author THD
+ * @creat 2022-10-23 22:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git,I'm thd");
+    }
+}
