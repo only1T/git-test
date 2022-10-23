bb5f6f728da9272fabe5ceebe977153cae8eabc8
thd <1187843791@qq.com>
2022-10-23 23:13:51 +0800

    third idea commit master

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -10,6 +10,8 @@ public class GitTest {
         System.out.println("hello git,I'm thd2");
         System.out.println("hello git,I'm thd3");
         System.out.println("hello git,I'm thd4");
+        System.out.println("master test");
+
 
     }
 }
